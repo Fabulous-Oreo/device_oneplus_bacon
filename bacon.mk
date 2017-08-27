20cf596f25ed02e246ec7721a8981256c70a4f3c
Kshitij Gupta <kshitijgm@gmail.com>
2017-08-27 14:32:51 +0000

    bacon: Add RenderScript HIDL packages
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -231,6 +231,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
