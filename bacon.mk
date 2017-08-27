1676820c7b104eb24135e5109c08485d78aff0ae
Kshitij Gupta <kshitijgm@gmail.com>
2017-08-27 13:50:53 +0000

    bacon: Remove ANT+
    - I highly doubt we need this
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -27,12 +27,6 @@ PRODUCT_PACKAGES += \
     messaging \
     Terminal
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
 # API (for CTS backward compatibility)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=19
@@ -174,7 +168,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
