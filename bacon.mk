e5a877222048dfccd5fa1476514def006d20eb02
Alex Cruz <du.alexcruz@gmail.com>
2017-09-01 02:52:47 +0200

    bacon: Add themes
    This makes use of AOSP's theme settings under Display. Adds two themes,
    a pixel one and a stock theme for users that want that AOSP look :-)
    
    Thanks to @Viperusgit for the heads on this and the prebuilt apk that was
    used to see how to go about recreating these type of themes
    
    Change-Id: I6cf94aa261fe153b640dd2209aad700eab7835e6
    
    [Kshitij Gupta]
      - We don't have a "ROM vendor" on AOSP.
      - Add it here, in the device tree.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -287,6 +287,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Themes
+PRODUCT_PACKAGES += \
+    PixelTheme \
+    Stock
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
+
 # Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine-8974.conf
