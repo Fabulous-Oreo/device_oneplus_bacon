2e90ab38a1586c49a5ea8ae4ad8f2b9d8870b74a
Kshitij Gupta <kshitijgm@gmail.com>
2017-11-02 14:14:50 +0000

    bacon: Move wcnss config to vendor
    - Needed for WiFi to work on Oreo's wcnss_service
    - Moving firmwares to vendor breaks WiFi after it turns off.
      Thus, leave it where it is.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- Android.mk
+++ Android.mk
@@ -157,10 +157,11 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wcd9320; \
 	ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
 		$(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_mad_audio.bin)
 
-# Create a link for the WCNSS config file, which ends up as a writable
-# version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+WCNSS_CFG_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+$(WCNSS_CFG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf /data/misc/wifi/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CFG_SYMLINK)
 
 endif
