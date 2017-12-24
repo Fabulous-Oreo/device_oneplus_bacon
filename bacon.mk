90ebb68ae3b207d44b0eba16bd2cd8467075df4e
Akshay Menon <deadlyindian@users.noreply.github.com>
2017-12-24 12:42:59 +0000

    Revert "bacon: Remove ANT+"
    This reverts commit 1676820c7b104eb24135e5109c08485d78aff0ae.

--- bacon.mk
+++ bacon.mk
@@ -27,6 +27,12 @@ PRODUCT_PACKAGES += \
     messaging \
     Terminal
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
+
 # API (for CTS backward compatibility)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=19
@@ -236,6 +242,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
