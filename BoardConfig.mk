6f5e90b3a2e5ddd887ac677c51bcac5b7218e80d
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-13 17:27:01 +0000

    bacon: Update for early-mounted system partition
     * This removes /system entry from fstab, since this partition
       is now early-mounted and the fstab entry is specified in device tree.
    
     * At the same time, also include a fully populated fstab, in order to avoid
       build breakage while generating recovery updater scripts.
    
    Signed-off-by: Akshay Menon <deadlyindian@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_RIL_VARIANT := caf
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.bacon
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.recovery
 
 # RPC
 TARGET_NO_RPC := true
