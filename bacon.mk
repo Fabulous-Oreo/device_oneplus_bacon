3d5e28cd09636f1221d6a7f24c9f726551d44f81
Bruno Martins <bgcngm@gmail.com>
2018-03-04 12:17:22 +0530

    msm8974-common: Binderize them all
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -56,7 +56,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
     audiod \
     audio.a2dp.default \
     audio_policy.msm8974 \
@@ -72,6 +74,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 PRODUCT_COPY_FILES += \
@@ -156,8 +159,10 @@ PRODUCT_PACKAGES += \
 # Gralloc
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -175,7 +180,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # IPC router config
 PRODUCT_COPY_FILES += \
