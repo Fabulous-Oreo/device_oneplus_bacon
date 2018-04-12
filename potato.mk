34832134fc4579baa6cc7b3fe57242713c0e52ee
Kshitij Gupta <kshitijgm@gmail.com>
2018-04-12 13:35:56 +0000

    bacon: Potato-ify
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- potato.mk
+++ potato.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-PRODUCT_NAME := aosp_bacon
+PRODUCT_NAME := potato_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := A0001
