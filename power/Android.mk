d1eb89ed9dfc30ec612c5fb776f2d955d1a46086
Kshitij Gupta <kshitijgm@gmail.com>
2018-04-12 13:52:59 +0000

    bacon: Import modified AOSP Power HAL
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

---
+++ power/Android.mk
@@ -0,0 +1,23 @@
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_PATH := $(call my-dir)
+
+    # HAL module implemenation stored in
+    # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+    include $(CLEAR_VARS)
+
+    ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+      LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+    endif
+
+    LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+    LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+    LOCAL_SRC_FILES := \
+                       power.c \
+
+    LOCAL_MODULE:= power.$(TARGET_BOARD_PLATFORM)
+    LOCAL_MODULE_TAGS := optional
+    include $(BUILD_SHARED_LIBRARY)
+  endif
+endif
