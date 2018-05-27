dced71aaa7d9b83b63bf26708f3dc92c1e894dbd
Kshitij Gupta <kshitijgm@gmail.com>
2018-05-27 17:13:46 +0000

    bacon: Append dtb and fix serial console on mainline
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,12 +45,11 @@ TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
-BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=ttyGS0,115200,n8 androidboot.console=ttyGS0 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
-BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
